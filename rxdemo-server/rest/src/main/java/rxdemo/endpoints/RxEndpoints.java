714f796b3af4901f8d9538827df46eaae9170a7d
Maikey <maikey.sip@proudnerds.com>
2022-02-24 14:32:51 +0100

    #5

--- rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxEndpoints.java
+++ rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxEndpoints.java
@@ -12,7 +12,7 @@ import static spark.Spark.*;
 
 // change to be deployed
 
-// Triggering a build #4
+// Triggering a build #5
 
 public class RxEndpoints {
    public static void initalizeEndpoints() {
